63c0839cbf9412deff8481b4895a714d21b5c714
growgavin4-sudo <growgavin4@gmail.com>
2026-02-08 10:01:00 -0600

    Modify TempConvert.py for user input and conversion
    Updated the script to include user input for Fahrenheit temperature and corrected the Celsius conversion formula.

--- TempConvert.py
+++ TempConvert.py
@@ -1,16 +1,18 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name:Gavin Grow
+#Date:2/8/26
+#Assignment:TempConv
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  tempF= float(input("Give me a temperature:"))
+  
+ 
 
-  tempC = tempF / 2
+  tempC=round( (tempF-32) *(5/9), 1)
 
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
