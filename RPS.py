22ee7b085ec0f99f3a6b9040a78e8bd7ea5060f9
growgavin4-sudo <growgavin4@gmail.com>
2026-02-08 10:52:55 -0600

    Implement game loop and player interaction for RPS
    Refactor RPS game to include player input and game loop.

--- RPS.py
+++ RPS.py
@@ -1,25 +1,48 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name:Gavin Grow
+#Date:2/8/26
+#Assignment:RPS
 import random
 
 def main():
-  wins = 0
-  ties = 0
-  losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
+    wins = 0
+    ties = 0
+    losses = 0
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+    playing = True
+## computer picks while user picks
+    while playing:
+        player = input("Choose your fighter! R, P, or S: ").upper()
+        computer = random.choice(['R', 'P', 'S'])
+## after they both pick , code will go through elif statements till finds one.
+        if computer == "R":
+            print("Computer chooses Rock")
+        elif computer == "P":
+            print("Computer chooses Paper")
+        else:
+            print("Computer chooses Scissors")
 
-  #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+        if computer == player:
+            print("You tied!")
+            ties += 1
+        elif (player == "R" and computer == "S") or \
+             (player == "P" and computer == "R") or \
+             (player == "S" and computer == "P"):
+            print("You beat the computer!")
+            wins += 1
+        else:
+            print("Computer beat you!")
+            losses += 1
+
+        again = input("Play again? (y/n): ").lower()
+        if again != "y":
+            playing = False
+##if they are done print this
+    print("\nFinal Stats")
+    print("Wins \t Ties \t Losses")
+    print("---- \t ---- \t ------")
+    print(wins, "\t", ties, "\t", losses)
+
 
 if __name__ == '__main__':
-  main()
+    main()
